68abb841f8bb5cb15626508ae75414d20255f5e7
jcooper <jcooper@cs.odu.edu>
2018-08-22 11:41:45 -0400

    Reddit rss scrapeage

--- src/main/scala/com/jrciii/rss/RssScraper.scala
+++ src/main/scala/com/jrciii/rss/RssScraper.scala
@@ -6,22 +6,19 @@ import scala.collection.JavaConverters._
 import java.net.URL
 
 import org.jsoup.Jsoup
-import org.jsoup.examples.HtmlToPlainText
 
 object RssScraper {
   def scrape(urls: List[String]) = {
     try {
       val sfi = new SyndFeedInput()
-      val formatter = new HtmlToPlainText
       urls.map(url => {
         val feed = sfi.build(new XmlReader(new URL(url)))
 
         val entries = feed.getEntries()
 
         entries.asScala.map(_.getContents.asScala.map(c => {
-          c.getValue + "\n===\n" +
-            formatter.getPlainText(Jsoup.parse(c.getValue))
-        })).mkString("\n\n")
+            Jsoup.parse(c.getValue).text()
+        }))
       })
     } catch {
       case e => throw new RuntimeException(e)
--- src/test/scala/com/jrciii/rss/RssScraperTest.scala
+++ src/test/scala/com/jrciii/rss/RssScraperTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.FreeSpec
 
 class RssScraperTest extends FreeSpec {
   "Run dat boi on /r/all" in {
-    println(RssScraper.scrape(List("https://www.reddit.com/r/all/comments.rss")))
+    RssScraper.scrape(List("https://www.reddit.com/r/all/comments.rss"))
+      .foreach(_.foreach(_.foreach(l => println(l + "\n"))))
   }
 }
